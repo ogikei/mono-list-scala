270e9f8557359f961a0f81a71bf44b682ff0fcf4
Keisuke Ogiwara <keisukeogiwara@KeisukenoMacBook-puro.local>
2018-08-28 19:14:50 +0900

    user login

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -2,15 +2,20 @@ package controllers
 
 import javax.inject._
 
+import jp.t2v.lab.play2.auth.OptionalAuthElement
 import play.api.i18n.I18nSupport
 import play.api.mvc._
+import services.UserService
 
 @Singleton
-class HomeController @Inject()(components: ControllerComponents)
+class HomeController @Inject()(val userService: UserService, components: ControllerComponents)
     extends AbstractController(components)
-        with I18nSupport {
+        with I18nSupport
+        with AuthConfigSupport
+        with OptionalAuthElement { // `AuthConfigSupport`,`OptionalAuthElement`をミックスイン
 
-  def index() = Action { implicit request: Request[AnyContent] =>
-    Ok(views.html.index())
+  def index: Action[AnyContent] = StackAction { implicit request => // `index`アクションを`Action`から`StackAction`に変更
+    Ok(views.html.index(loggedIn)) // `views.html.index()`から`views.html.index(loggedIn)`に変更
   }
+
 }
