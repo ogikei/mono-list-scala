8dd2aad199f2bafa3abbbabba3e64b976d5ecc90
Keisuke Ogiwara <keisukeogiwara@KeisukenoMacBook-puro.local>
2018-08-28 18:23:53 +0900

    add create db scripts

---
+++ conf/db/migration/default/common/V2__Add_Users.sql
@@ -0,0 +1,6 @@
+INSERT INTO users (name, email, password, create_at, update_at) VALUES ('test1', 'test1@test.com', '$2a$10$PbCjaDmzPlOASSe64zoS1e8/iV2Xs2ATdZcPyLaron6QjMMzpcj/y', now(), now());
+INSERT INTO users (name, email, password, create_at, update_at) VALUES ('test2', 'test2@test.com', '$2a$10$PbCjaDmzPlOASSe64zoS1eWkMTVopJQm0kU4dRTaIKhaAGQL0TF.2', now(), now());
+INSERT INTO users (name, email, password, create_at, update_at) VALUES ('test3', 'test3@test.com', '$2a$10$PbCjaDmzPlOASSe64zoS1eS2kNjtls0a7Psi/VPwJthA01Oc2b.We', now(), now());
+INSERT INTO users (name, email, password, create_at, update_at) VALUES ('test4', 'test4@test.com', '$2a$10$PbCjaDmzPlOASSe64zoS1eWT6RDafOoIlz2p5bJrE4qr.lcSkPAfW', now(), now());
+INSERT INTO users (name, email, password, create_at, update_at) VALUES ('test5', 'test5@test.com', '$2a$10$PbCjaDmzPlOASSe64zoS1eIaBK9paEuTUS0yrjVrN7l6NLyRKQmBe', now(), now());
+INSERT INTO users (name, email, password, create_at, update_at) VALUES ('test6', 'test6@test.com', '$2a$10$PbCjaDmzPlOASSe64zoS1eleRJBmV/NSnmMYK8KbyTLeUA.iVh5yu', now(), now());
